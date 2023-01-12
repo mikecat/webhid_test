9122f0d982a3bd7e380e162e9661227d65e02730
MikeCAT <mikecat@users.noreply.github.com>
2023-01-13 02:51:25 +0900

    add device table

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function() {
-	const h = new HexArea(true);
-	document.body.appendChild(h.node);
-	let testData = "";
-	for (let i = 0; i < 32; i++) {
-		testData += String.fromCharCode((Math.random() * 256) >>> 0);
-	}
-	h.data = testData;
+	const addDeviceButton = document.getElementById("add-device-button");
+	const deviceTable = document.getElementById("device-table");
+
+	addDeviceButton.addEventListener("click", function() {
+		const d = document.createElement("div");
+		d.setAttribute("class", "device-table-cell");
+		const h = new HexArea(true);
+		d.appendChild(h.node);
+		deviceTable.appendChild(d);
+		let testData = "";
+		for (let i = 0; i < 32; i++) {
+			testData += String.fromCharCode((Math.random() * 256) >>> 0);
+		}
+		h.data = testData;
+	});
 });
